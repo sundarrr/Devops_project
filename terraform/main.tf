820cdcb84b23df3e331a84819a7448c7f7b64652
sundarrr <sundarrathinavel1@gmail.com>
2025-05-05 17:42:40 -0400

    fixed typo in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,7 +42,7 @@ resource "aws_security_group" "maingroup" {
             cidr_blocks = ["0.0.0.0/0"]
             description = ""
             from_port = 0
-            ipv6_cidr_blockc = []
+            ipv6_cidr_blocks = []
             prefix_list_ids = []
             protocol = "-1"
             security_groups = []
@@ -55,7 +55,7 @@ resource "aws_security_group" "maingroup" {
             cidr_blocks = ["0.0.0.0/0"]
             description = ""
             from_port = 22
-            ipv6_cidr_blockc = []
+            ipv6_cidr_blocks = []
             prefix_list_ids = []
             protocol = "tcp"
             security_groups = []
@@ -65,7 +65,7 @@ resource "aws_security_group" "maingroup" {
         {
             cidr_blocks = ["0.0.0.0/0"]
             from_port = 80
-            ipv6_cidr_blockc = []
+            ipv6_cidr_blocks = []
             prefix_list_ids = []
             protocol = "tcp"
             security_groups = []
