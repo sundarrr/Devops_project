7bc2abae7a196b70f6ff0080b88e04044c0c78e9
sundarrr <sundarrathinavel1@gmail.com>
2025-05-05 23:00:39 -0400

    made associate ip_adedress true

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,6 +20,7 @@ resource "aws_instance" "server" {
     key_name = aws_key_pair.deployer.key_name
     vpc_security_group_ids = [aws_security_group.maingroup.id]
     iam_instance_profile = aws_iam_instance_profile.ec2-profile.name
+    associate_public_ip_address = true
     connection{
         type = "ssh"
         host = self.public_ip
